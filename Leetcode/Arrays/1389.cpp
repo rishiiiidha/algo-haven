bbc82c31ccb531fff76c049490f65270d66db8b8
rishidha <rishidha04@gmail.com>
2024-06-08 22:57:51 +0530

    arrays

---
+++ Leetcode/Arrays/1389.cpp
@@ -0,0 +1,15 @@
+// class Solution {
+// public:
+//     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+//       int n = nums.size();
+//       vector<int>ans;
+//       for(int i=0;i<n;i++){
+//          auto it = ans.begin();
+//          ans.insert(it+index[i],nums[i]);
+//       }
+//       return ans;
+//     }
+// };
+
+// Time complexity : O(n*n)
+// Space complexity : O(n)
