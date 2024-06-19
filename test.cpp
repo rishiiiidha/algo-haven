4167964c6b51495b0b2c817058672c61fcb4685a
rishidha <rishidha04@gmail.com>
2024-06-19 17:50:17 +0530

    practice

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(),(x).end()
+#define endl '\n'
+#define int int64_t
+const int MOD = 1e9 + 7;
+const int INF = LONG_MAX >> 1;
+void solve(){
+    int64_t a;
+    cin >> a;
+    cout << a;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t;
+    t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
