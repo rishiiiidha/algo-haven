fd69263792248023fbbf14f35837e404d52a1110
Sri Venkata Rishidha Addanki <rishidha04@gmail.com>
2025-01-25 01:38:57 +0530

    just one more to go

--- StriverA2ZSheet2025/patterns/20.cpp
+++ StriverA2ZSheet2025/patterns/20.cpp
@@ -6,29 +6,30 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << "* ";
         }
         // spaces
-        for (int j = 0; j < 2*i; j++)
+        for (int j = 0; j < 2 * (n - i - 1); j++)
         {
             cout << "  ";
         }
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << "* ";
         }
         cout << endl;
     }
-    for (int i = n-1; i >=0; i--)
+    n--;
+    for (int i = 0 ; i < n; i++)
     {
         for (int j = 0; j < n - i; j++)
         {
             cout << "* ";
         }
         // spaces
-        for (int j = 0; j < 2 * i; j++)
+        for (int j = 0; j < 2 * (i+1); j++)
         {
             cout << "  ";
         }
